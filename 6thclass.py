3e6a5e3118401a7aa521d35c4f68e2cb08b4690c
vijayaratnakumari <vijaya@sarithm.com>
2022-03-28 16:57:28 +0530

    add bitwise operator program

--- 6thclass.py
+++ 6thclass.py
@@ -1,6 +1,17 @@
-n =int (input("enter number:"))
+'''n =int (input("enter number:"))
 s = 0
 
 for i in range(1, n+1):
     s = s+i
-print("sum of ", n, "numbers:", s)
+print("sum of ", n, "numbers:", s)'''
+
+#bitwise operators
+a=10
+b=12
+print(bin(a))
+print(bin(b))
+print(bin(a&b))
+print(bin(a|b))
+print(bin(a^b))
+print(bin(~a))
+print(bin(~b))
