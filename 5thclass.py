7d56ce4eee3fb9252e5f5281df3a5ab60e851fc8
vijayaratnakumari <vijaya@sarithm.com>
2022-03-28 15:32:29 +0530

    string program

---
+++ 5thclass.py
@@ -0,0 +1,6 @@
+d={1:"rani",1:"kumari",2:"esther",3:"esther",4:"vijaya"}
+
+print(d)
+print(type(d))
+d[3]="souji"
+print(d)
---
+++ text.py
@@ -0,0 +1,6 @@
+
+for i in range(1,11):
+    if i ==3 or i ==5 or i ==8:
+        continue
+        print(i, end="")
+
